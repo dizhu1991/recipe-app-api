e0bc00e2025c00fb0ff035dc90e083a502bc3537
Di Zhu <di.zhu.jude@gmail.com>
2019-07-06 00:18:18 +1200

    first commit on tutorial

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+"""URLs for pages app."""
+from django.urls import path
+
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.homePageView, name='home')
+]
---
+++ pages/views.py
@@ -0,0 +1,7 @@
+"""Views for pages app."""
+
+from django.http import HttpResponse
+
+
+def homePageView(request):
+    return HttpResponse('Hello World!')
